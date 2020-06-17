413a4a50df2bbb3c1f8588e586d23b5bbcdddb88
emmaodia <odia.emma@gmail.com>
2020-06-17 04:12:44 +0100

    Refactored Footer.

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,10 +1,4 @@
-import React, { useState } from 'react';
-import {
-  Navbar,
-  Nav,
-  NavItem,
-  NavLink
-} from 'reactstrap';
+import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHandHoldingHeart, faCopyright } from '@fortawesome/free-solid-svg-icons'
@@ -13,14 +7,28 @@ import { faHandHoldingHeart, faCopyright } from '@fortawesome/free-solid-svg-ico
 const element = <FontAwesomeIcon icon={faHandHoldingHeart} />
 const copyright = <FontAwesomeIcon icon={faCopyright} />
 
-const Footer = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
+const Footer = () => {
 
   return (
     <div>
-      <Navbar color="light" light expand="md">
+      <ul className="flex justify-between">
+        <li className="mr-6">
+          <a className="text-blue-500 hover:text-blue-800" href="https://fb.com/groups/devclagos" target="_blank">Made with {element} by DevC Lagos Community</a>
+        </li>
+        <li className="mr-6">
+          <p className="text-blue-500 hover:text-blue-800">License Apache-2.0</p>
+        </li>
+        <li className="mr-6">
+          <p className="text-gray-400 cursor-not-allowed" href="#">{copyright} 2020</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
+
+{/* <Navbar color="light" light expand="md">
         <Nav className="center">
         <NavItem>
             <NavLink href="https://fb.com/groups/devclagos" target="_blank">Made with {element} by DevC Lagos Community</NavLink>
@@ -29,9 +37,4 @@ const Footer = (props) => {
           <NavLink disabled href="#">{copyright} 2020</NavLink>
         </NavItem>
         </Nav>
-      </Navbar>
-    </div>
-  );
-}
-
-export default Footer;
+      </Navbar> */}
