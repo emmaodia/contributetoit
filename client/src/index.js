7f615cfd18286aa81a362684e8eb51b18eb48c78
emmaodia <odia.emma@gmail.com>
2020-06-17 04:19:27 +0100

    Updated index.js files.

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/main.css';
+import './styles/global.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
