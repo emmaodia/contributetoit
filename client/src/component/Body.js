96177f993006b3bb20a30b55ff9fcf5cf3d43694
emmaodia <odia.emma@gmail.com>
2020-06-17 04:12:59 +0100

    Refactored Body.

--- client/src/component/Body.js
+++ client/src/component/Body.js
@@ -1,67 +1,35 @@
-import React, { useState } from 'react';
-import {
-  Container, Row, Col
-} from 'reactstrap';
-
-const items = [
-  {
-    src: require('../images/BlackManBlackWomanUsingLaptop.jpg'),
-    altText: 'Slide 1',
-    caption: 'Slide 1'
-  },
-  {
-    src: require('../images/BlackManandWomanWorkingonCalendar.jpg'),
-    altText: 'Slide 2',
-    caption: 'Slide 2'
-  },
-  {
-    src: require('../images/TwoBlackMenUsingLaptop.jpg'),
-    altText: 'Slide 3',
-    caption: 'Slide 3'
-  }
-];
-
-const see = {
-              height: "100%",
-              padding: "100px",
-              margin: "10px"
-            }
-
-const imgStyle = {
-                  display: "block",
-                  width: "auto",
-                  height: "auto",
-                  "max-width": "100%",
-                  "max-height": "90%",
-                  margin: "20px auto"
-                };
+import React from 'react';
 
 const Body = () => {
 
-
-
-
   return (
-    <div style={see}>
-      <Container>
-        <Row>
-          <Col xs="6"><img src={require('../images/BlackManBlackWomanUsingLaptop.jpg')} style={imgStyle} /></Col>
-          <Col xs="6">Create</Col>
-        </Row>
-        <Row>
-          <Col xs="6">Contribute</Col>
-          <Col xs="6"><img src={require('../images/BlackManandWomanWorkingonCalendar.jpg')} style={imgStyle} /></Col>
-        </Row>
-        <Row>
-          <Col xs="6"><img src={require('../images/TwoBlackMenUsingLaptop.jpg')} style={imgStyle} /></Col>
-          <Col xs="6">Consume</Col>
-        </Row>
-        <Row>
-          <Col xs="6">Critique</Col>
-          <Col xs="6"><img src={require('../images/BlackMillennialsatOffice.jpg')} style={imgStyle} /></Col>
-        </Row>
-      </Container>
-    </div>
+    <>
+    <section>
+      <div className="flex flex-wrap mt-20">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400"><img src={require('../images/BlackManBlackWomanUsingLaptop.jpg')} alt="Black Woman Using Laptop illustration" /></div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4">
+          <div className="lg:w-4/12 px-4 ml-auto mb-6 text-center">
+            <h2 className="font-extrabold  text-4xl">Create</h2>
+            <h3 className=""></h3>
+            <p className=""></p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400">Contribute</div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400"><img src={require('../images/BlackManandWomanWorkingonCalendar.jpg')} alt="Black Man and Woman Workingon Calendar illustration" /></div>
+      </div>
+      <div className="flex flex-wrap">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400"><img src={require('../images/TwoBlackMenUsingLaptop.jpg')} alt="Two Black Men Using Laptop illustration" /></div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400">Consume</div>
+      </div>
+      <div className="flex flex-wrap">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400">Critique</div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400"><img src={require('../images/BlackMillennialsatOffice.jpg')} alt="Black Millennials at Office illustration" /></div>
+      </div>
+    </section>
+    </>
   );
 }
 
