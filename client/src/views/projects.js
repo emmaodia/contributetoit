ae8d2b39ab5bcab671cfa26fd20d61b0e11145da
emmaodia <odia.emma@gmail.com>
2020-06-21 13:43:26 +0100

    Set up Project Views Page.

---
+++ client/src/views/projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Projects from '../component/Project';
+// import { axios } from '../helpers/axios';
+
+function Project() {
+  
+
+  return (
+   <div>Working...</div>
+  );
+}
+
+export default Project;
