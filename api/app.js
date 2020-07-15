fa4b1a439e598790012b101686b41f9972cc7970
emmaodia <odia.emma@gmail.com>
2020-07-15 23:44:58 +0100

    Implemented Github OAuth.

--- api/app.js
+++ api/app.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 // const logger = require('morgan');
@@ -71,7 +70,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', projectRouter);
+app.use('/api/v1', projectRouter);
 
 // Database Configuration
 const url = 'mongodb://localhost:27017/contributetoit-api';
