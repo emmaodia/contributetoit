17c638183209d8c0d61524a47d49d60087777d40
emmaodia <odia.emma@gmail.com>
2020-11-13 01:46:42 +0100

    CHORE: Add Child elements to Props.

--- client/src/views/projects.js
+++ client/src/views/projects.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import Projects from '../component/Body';
+import Projects from '../component/Projects';
 // import { axios } from '../helpers/axios';
 
 function Project() {
-  const exReq = `https://localhost:9000/api/v1/`
+  const URL_PATH = `http://localhost:9000/api/v1/projects/`
 
   const [projects, setProjects] = useState([]);
   const [fetching, setFetching] = useState(true);
 
   useEffect(() => {
-    // fetch(exReq).then((projects) => {
+    // fetch(URL_PATH).then((projects) => {
     //   setProjects(projects);
     //   setFetching(false);
     //});
     const getProfiles = async () => {
-    const response = await fetch(exReq)
+    const response = await fetch(URL_PATH)
     const data = await response.json();
       
-    setProjects(data.response);
+    setProjects(data);
     setFetching(false);
-    console.log(data.response);
+    console.log(data);
   }
   getProfiles();
-  }, [exReq, setProjects]);
+  }, [setProjects]);
 
   if (fetching) return <p>fetching...</p>;
 
@@ -30,7 +30,7 @@ function Project() {
    
    <section>
      <div>Working...{console.log("Im here")}</div>
-   <article className='pt-10 pb-8'>
+    <article className='pt-10 pb-8'>
      <h1 className='text-5xl font-light'>Projects</h1>
    </article>
 
@@ -38,11 +38,13 @@ function Project() {
      {projects.map((project) => (
        <Projects
           key={project._id}
-          link={`project/${project._id}`}
+          link={`${URL_PATH}${project._id}`}
+          title={project.title}
           msg={project.msg}
+          msg={project.url}
        />
      ))}
-   </section>
+   </section> 
  </section>
   );
 }
