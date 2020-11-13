a15c6c63f5f70849bb282d4dafdee9ca3e6201c2
emmaodia <odia.emma@gmail.com>
2020-11-13 01:47:23 +0100

    FIX...

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import Home from './views/home'
+import { BrowserRouter } from "react-router-dom";
+import { Switch, Route } from 'react-router-dom';
+import routes from './routes/index';
+import Header from './component/Header';
+// import Home from './views/home'
 import './App.css';
 
 function App() {
   return (
-   <Home />
+    <BrowserRouter>
+      <Header />
+      <section className='mobile:px-10 px-4 pb-10 flex justify-center'>
+      <section className='w-fw'>
+        <Switch>
+          {routes.map(({ path, component: Component, exact}, index ) => (
+            <Route 
+              key={index}
+              path={path}
+              exact={exact}
+              render={(props) => <Component {...props} />}
+            />
+          ))}
+        </Switch>
+      </section>
+    </section>
+      {/* <Home /> */}
+    </BrowserRouter>
   );
 }
 
