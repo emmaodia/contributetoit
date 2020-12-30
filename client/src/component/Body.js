4d9590d4295f52dca92e1483eacc53d9a1be352f
emmaodia <odia.emma@gmail.com>
2020-12-30 23:15:06 +0100

    ui: Changed landing page content

--- client/src/component/Body.js
+++ client/src/component/Body.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Body = () => {
-
   return (
     <>
-    <section>
-      <div className="flex flex-wrap mt-20">
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400"><img src={require('../images/BlackManBlackWomanUsingLaptop.jpg')} alt="Black Woman Using Laptop illustration" /></div>
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4">
-          <div className="lg:w-4/12 px-4 ml-auto mb-6 text-center">
-            <h2 className="font-extrabold  text-4xl">Create</h2>
-            <h3 className=""></h3>
-            <p className=""></p>
-            <p></p>
+      <div className="flex flex-rowx justify-between items-center mt-20">
+        <div className="w-full">
+          <img
+            src={require("../images/BlackManBlackWomanUsingLaptop.jpg")}
+            alt="Black Woman Using Laptop illustration"
+          />
+        </div>
+        <div className="w-full">
+          <div class="border-solid border-2 rounded-lg border-light-blue-500">
+            <div className="text-center inline-block align-middle m-16 font-mono">
+              This is a platform where Open Source creators can add their
+              projects and everyone can "Contribute to it"
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-wrap">
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400">Contribute</div>
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400"><img src={require('../images/BlackManandWomanWorkingonCalendar.jpg')} alt="Black Man and Woman Workingon Calendar illustration" /></div>
+      <div>
+        <p>
+          Add your Project to the list, find a Project to "Contribute to it!"
+        </p>
       </div>
-      <div className="flex flex-wrap">
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400"><img src={require('../images/TwoBlackMenUsingLaptop.jpg')} alt="Two Black Men Using Laptop illustration" /></div>
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400">Consume</div>
-      </div>
-      <div className="flex flex-wrap">
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400">Critique</div>
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-2/4 xl:w-3/6 mb-4 bg-gray-400"><img src={require('../images/BlackMillennialsatOffice.jpg')} alt="Black Millennials at Office illustration" /></div>
-      </div>
-    </section>
     </>
   );
-}
+};
 
 export default Body;
+
+/**
+ * This is a platform where Open Source creators can add the projects and everyone can "Contribute to it"
+ *
+ * Add your Project to the list, find a Project to "Contribute to it!"
+ */
