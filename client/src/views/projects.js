01978d3f7cf94bf8bc14e0160a5ffb693e1303c2
emmaodia <odia.emma@gmail.com>
2020-12-30 23:17:18 +0100

    ui: fix...

--- client/src/views/projects.js
+++ client/src/views/projects.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import Projects from '../component/Projects';
+import React, { useEffect, useState } from "react";
+import Projects from "../component/Projects";
 // import { axios } from '../helpers/axios';
 
 function Project() {
-  const URL_PATH = `http://localhost:9000/api/v1/projects/`
+  const URL_PATH = `http://localhost:9000/api/v1/projects/`;
 
   const [projects, setProjects] = useState([]);
   const [fetching, setFetching] = useState(true);
@@ -14,38 +14,37 @@ function Project() {
     //   setFetching(false);
     //});
     const getProfiles = async () => {
-    const response = await fetch(URL_PATH)
-    const data = await response.json();
-      
-    setProjects(data);
-    setFetching(false);
-    console.log(data);
-  }
-  getProfiles();
-  }, [setProjects]);
+      const response = await fetch(URL_PATH);
+      const data = await response.json();
+
+      setProjects(data);
+      setFetching(false);
+      console.log(data);
+    };
+    getProfiles();
+  }, [setProjects, URL_PATH]);
 
   if (fetching) return <p>fetching...</p>;
 
   return (
-   
-   <section>
-     <div>Working...{console.log("Im here")}</div>
-    <article className='pt-10 pb-8'>
-     <h1 className='text-5xl font-light'>Projects</h1>
-   </article>
+    <section>
+      <div>Working...{console.log("Im here")}</div>
+      <article className="pt-10 pb-8">
+        <h1 className="text-5xl font-light">Projects</h1>
+      </article>
 
-   <section className='flex flex-wrap'>
-     {projects.map((project) => (
-       <Projects
-          key={project._id}
-          link={`${URL_PATH}${project._id}`}
-          title={project.title}
-          msg={project.msg}
-          msg={project.url}
-       />
-     ))}
-   </section> 
- </section>
+      <section className="flex flex-wrap">
+        {projects.map((project) => (
+          <Projects
+            key={project._id}
+            link={`${URL_PATH}${project._id}`}
+            title={project.title}
+            msg={project.msg}
+            msg={project.url}
+          />
+        ))}
+      </section>
+    </section>
   );
 }
 
