13a5647f4c2556a0d4b7113ca2b4f382b1f9bfb2
emmaodia <odia.emma@gmail.com>
2020-12-30 23:17:02 +0100

    ui: fix...t

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 // import Header from '../component/Header';
-import Body from '../component/Body';
-import Footer from '../component/Footer';
+import Body from "../component/Body";
+import Footer from "../component/Footer";
 
 function Home() {
   return (
     <>
       {/* <Header /> */}
-      <Body />
-      <Footer />
+      <div class="container mx-auto px-24">
+        <div className="flex flex-col min-h-screen">
+          <div className="flex-grow">
+            <Body />
+          </div>
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
