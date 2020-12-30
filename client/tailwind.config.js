9023c8e148c4c8304bd47de31f6c356b2e76bdef
emmaodia <odia.emma@gmail.com>
2020-12-30 23:16:24 +0100

    ui: set body in configuring to center content

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,39 +1,42 @@
 module.exports = {
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    container: {
+      center: true,
     },
     fontFamily: {
-      display: ['Montserrat', 'sans-serif'],
-      body: ['Montserrat', 'sans-serif'],
+      display: ["Montserrat", "sans-serif"],
+      body: ["Montserrat", "sans-serif"],
     },
     borderWidth: {
-      default: '1px',
-      '0': '0',
-      '2': '2px',
-      '4': '4px',
+      default: "1px",
+      0: "0",
+      2: "2px",
+      4: "4px",
     },
     extend: {
       colors: {
-        cyan: '#9cdbff',
+        cyan: "#9cdbff",
       },
       spacing: {
-        '96': '24rem',
-        '128': '32rem',
-        fw: '1440px',
+        96: "24rem",
+        128: "32rem",
+        fw: "1440px",
       },
       height: {
-        '72': '18rem',
-        '112': '28rem',
+        72: "18rem",
+        112: "28rem",
       },
       maxHeight: {
-        '96': '24rem',
+        96: "24rem",
       },
     },
     variants: {},
     plugins: [],
-  }
-}
+  },
+};
